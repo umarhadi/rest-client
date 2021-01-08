43f62ab8bab260abfcbf813549a08f1a1e82cbb9
Umar Hadi Siswanto <umar.hadi.siswanto@student.uty.ac.id>
2021-01-08 16:16:00 +0800

    final :)

---
+++ app/src/main/java/com/umar/restclient/UmarActivity.kt
@@ -0,0 +1,11 @@
+package com.umar.restclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UmarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_umar)
+    }
+}
